a41bf295cfa686b10d191756e129d02ceace99ed
Matt Kennedy <mjknj18@gmail.com>
2019-11-07 20:57:09 -0500

    Updated SQL Queries.

--- Employee_Data_Queries_Main.sql
+++ Employee_Data_Queries_Main.sql
@@ -75,7 +75,7 @@ FROM dept_emp;
 --Display Employee Last Name/First Name/Gender/Salary
 SELECT employees.last_name, employees.first_name, employees.gender, salaries.salary
 FROM employees
-LEFT JOIN salaries ON employees.emp_no = salaries.emp_no;
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
 --Display Employees Hired in 1986
 SELECT last_name, first_name, hire_date
@@ -86,14 +86,14 @@ WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986';
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name,
 	employees.first_name, dept_manager.from_date, dept_manager.to_date
 FROM dept_manager
-LEFT JOIN departments ON dept_manager.dept_no = departments.dept_no
-LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no;
+INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
+INNER JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
 --Display Employee Emp #/Last Name/First Name/Department Name
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
-LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
-LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no;
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no;
 
 --Display Employees with First Name of "Hercules" & Last Name Starting with "B"
 SELECT last_name, first_name
@@ -104,15 +104,15 @@ AND LEFT(last_name, 1) = 'B';
 --Display Sales Department Employee Emp #/Last Name/First Name/Department Name
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
-LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
-LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
 WHERE departments.dept_name = 'Sales';
 
 --Display Sales/Development Department Employee Dep #/Last Name/First Name/Department Name
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
-LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
-LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
 WHERE departments.dept_name = 'Sales'
 OR departments.dept_name = 'Development';
 
